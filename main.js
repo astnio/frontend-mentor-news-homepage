d9f092197bbfc682747e730f3b7d47ee4b6d1fd1
astn <github@msg.austinh.io>
2024-10-14 00:08:34 -0700

    Fixed resizing issue on mobile nav

--- main.js
+++ main.js
@@ -5,33 +5,46 @@ const navOverlay = document.querySelector('.nav-overlay');
 
 let menuActive = false;
 
-function adjustMenu() {
-  const breakpoint = 1160;
-  if (window.innerWidth >= breakpoint) {
-    navList.style.transform = 'translateX(0%)';
-    navOverlay.dataset.active = 'false';
-    navButtonIcon.src = './assets/images/icon-menu.svg';
-    navButton.style.position = 'absolute';
-    menuActive = false;
-  } else if (window.innerWidth <= breakpoint) {
-    navList.style.transform = 'translateX(100%)';
-  }
-}
-
-navButton.addEventListener('click', () => {
-  menuActive = !menuActive;
-
-  if (menuActive) {
+function updateMenu() {
+  console.log(`Menu is active: ${menuActive}`);
+  if (menuActive === false) {
     navList.style.transform = 'translateX(100%)';
     navOverlay.dataset.active = 'false';
     navButtonIcon.src = './assets/images/icon-menu.svg';
     navButton.style.position = 'absolute';
-  } else if (!menuActive) {
+  } else if (menuActive === true) {
     navList.style.transform = 'translateX(0%)';
     navOverlay.dataset.active = 'true';
     navButtonIcon.src = './assets/images/icon-menu-close.svg';
     navButton.style.position = 'fixed';
   }
+}
+
+function adjustMenu() {
+  const breakpoint = 1160;
+  if (window.innerWidth >= breakpoint) {
+    menuActive = false;
+    navList.style.transform = 'translateX(0%)';
+    navOverlay.dataset.active = 'false';
+    navButtonIcon.src = './assets/images/icon-menu.svg';
+    navButton.style.position = 'absolute';
+  } else {
+    if (!menuActive) {
+      navList.style.transform = 'translateX(100%)';
+    }
+  }
+}
+
+navButton.addEventListener('click', () => {
+  menuActive = !menuActive;
+  updateMenu();
+});
+
+navOverlay.addEventListener('click', () => {
+  menuActive = !menuActive;
+  updateMenu();
 });
 
 window.addEventListener('resize', adjustMenu);
+
+adjustMenu();
